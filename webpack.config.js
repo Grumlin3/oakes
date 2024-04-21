6c6390840ec9c55e9b478ea83996a1d0b669f6f4
Merit <merit@flatfile.io>
2024-04-21 12:10:16 -0700

    Restore path temporarily
    Signed-off-by: Merit <merit@flatfile.io>

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.bundle.js',
-    publicPath: '/'
+    publicPath: process.env.LOCAL ? '/' : '/oakes/'
   },
 }
