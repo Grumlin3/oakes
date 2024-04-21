57142783dfd7d62de7e53b9f630012e91f14b486
Merit <merit@flatfile.io>
2024-04-21 12:04:14 -0700

    Update path
    Signed-off-by: Merit <merit@flatfile.io>

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.bundle.js',
-    publicPath: process.env.LOCAL ? '/' : '/oakes/'
+    publicPath: '/'
   },
 }
